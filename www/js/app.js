1d32241a87184735de75cf4887854ab9bfceae31
coharris_asus <christianoharris@gmail.com>
2015-10-06 11:35:38 -0400

    -added ui and sass

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('esiHopeBar', ['ionic', 'esiHopeBar.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,11 +17,101 @@ angular.module('starter', ['ionic'])
     }
   });
 })
-.controller('DashCtrl',function($scope) {
-  $scope.launchSite = function() {
-    window.open('http://www.hopebar.com', '_system', 'location=yes');
-  };
-    $scope.launchBuy = function() {
-      window.open('http://www.hopebar.com/shop/', '_system', 'location=yes')
-    }
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'HTMLs/menu.html',
+      controller: 'AppCtrl'
+    })
+
+    .state('app.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+          templateUrl: 'HTMLs/login.html'
+        }
+      }
+
+    })
+
+    .state('app.home', {
+      url: '/home',
+      views: {
+        'menuContent':{
+          templateUrl: 'HTMLs/home.html'
+        }
+      }
+    })
+
+    .state('app.bar', {
+      url: '/bar',
+      views: {
+        'menuContent': {
+          templateUrl: 'HTMLs/bar.html'
+        }
+      }
+    })
+
+    .state('app.nourish', {
+      url: '/nourish',
+      views: {
+        'menuContent': {
+          templateUrl: 'HTMLs/nourish.html'
+        }
+      }
+    })
+
+    .state('app.plan', {
+      url: '/plan',
+      views: {
+        'menuContent': {
+          templateUrl: 'HTMLs/plan.html'
+        }
+      }
+    })
+
+    .state('app.resources', {
+      url: '/resources',
+        views: {
+          'menuContent': {
+            templateUrl: 'HTMLs/resources.html'
+          }
+        }
+    })
+
+    .state('app.settings', {
+      url: '/settings',
+      views: {
+        'menuContent': {
+          templateUrl: 'HTMLs/settings.html'
+        }
+      }
+    })
+
+    .state('app.shop', {
+      url: '/shop',
+      views: {
+        'menuContent': {
+          templateUrl: 'HTMLs/shop.html'
+        }
+      }
+    })
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+
+  $urlRouterProvider.otherwise('/app/home');
+
+
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,21 @@
-angular.module('starter.controllers', [])
+/**
+ * Created by chris_000 on 10/2/2015.
+ */
+angular.module('esiHopeBar.controllers', [])
+.controller('AppCtrl', function($scope) {
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+
+  })
+
+
+.controller('DashCtrl',function($scope) {
+  $scope.launchSite = function() {
+    window.open('http://www.hopebar.com', '_system', 'location=yes');
   };
-})
+  $scope.launchBuy = function() {
+    window.open('http://www.hopebar.com/shop/', '_system', 'location=yes')
+  }
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });

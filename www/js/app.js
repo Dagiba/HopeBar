89719b6e8462a2e99aa808b889b84aac1581971e
miller <fasha1000@gmail.com>
2015-10-06 07:22:06 -0400

    updated sharing functionality

--- www/js/app.js
+++ www/js/app.js
@@ -26,26 +26,73 @@ var ionicApp = angular.module('starter', ['ionic', 'ngCordova'])
       window.open('http://www.hopebar.com/shop/', '_system', 'location=yes')
     }
 });
+ionicApp.controller('MyCtrl', function($scope, $cordovaSocialSharing) {
 
-ionicApp.controller("ExampleController", function($scope, $cordovaSocialSharing) {
-	$scope.shareAnywhere = function() {
-             $cordovaSocialSharing.share("This is your message",
-	     "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
-	}
+  $cordovaSocialSharing
+    .share(message, subject, file, link) // Share via native share sheet
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
 
-	$scope.shareViaTwitter = function() {
-    		$cordovaSocialSharing.shareViaTwitter("Check out this cool app I'm using called IonicProject for "
-		+ device.platform, null,
-		device.platform == "Android" ? "GOOGLE_PLAY_URL" : "ITUNES_URL");
-	}
+  $cordovaSocialSharing
+    .shareViaTwitter(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
 
-	$scope.shareViaTwitter = function(message, image, link) {
-		$cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
-			$cordovaSocialSharing.shareViaTwitter(message, image, link);
-		}, function(error) {
-			alert("Cannot share on Twitter");
-		});
-	
-	}
+  $cordovaSocialSharing
+    .shareViaWhatsApp(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .shareViaFacebook(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  // access multiple numbers in a string like: '0612345678,0687654321'
+  $cordovaSocialSharing
+    .shareViaSMS(message, number)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+// toArr, ccArr and bccArr must be an array, file can be either null, string or array
+  $cordovaSocialSharing
+    .shareViaEmail(message, subject, toArr, ccArr, bccArr, file)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .canShareVia(socialType, message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .canShareViaEmail()
+    .then(function(result) {
+      // Yes we can
+    }, function(err) {
+      // Nope
+    });
 });
+
 

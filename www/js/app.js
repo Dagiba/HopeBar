0179f0802c21baae836a9e88c02763ebc8d3c6a3
miller <fasha1000@gmail.com>
2015-10-20 13:21:15 -0400

    fixing sharing and local storage func

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,8 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var ionicApp = angular.module('starter', ['ionic', 'ngCordova'])
+var example = angular.module('starter', ['ionic', 'ngCordova'])
+var db = null;
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -15,8 +17,37 @@ var ionicApp = angular.module('starter', ['ionic', 'ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+	db = $cordovaSQLite.openDB({ name."my.db"});
+	$cordovaSQLite.execute(db, "create table if doesn't exist");
   });
-})
+});
+ 
+
+example.controller("ExampleController", function($scope, $cordovaSQLite) {
+ 
+    $scope.insert = function(firstname, lastname) {
+        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+        $cordovaSQLite.execute(db, query, [firstname, lastname]).then(function(res) {
+            console.log("INSERT ID -> " + res.insertId);
+        }, function (err) {
+            console.error(err);
+        });
+    }
+ 
+    $scope.select = function(lastname) {
+        var query = "SELECT firstname, lastname FROM people WHERE lastname = ?";
+        $cordovaSQLite.execute(db, query, [lastname]).then(function(res) {
+            if(res.rows.length > 0) {
+                console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+            } else {
+                console.log("No results found");
+            }
+        }, function (err) {
+            console.error(err);
+        });
+    }
+ 
+});
 
 .controller('DashCtrl',function($scope) {
   $scope.launchSite = function() {
